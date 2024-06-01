5e580912a10cd60d121eb8ddc0f3e1129b605bb8
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-06-01 18:36:21 +0300

    Create ImageModal.tsx

---
+++ src/components/ImageModal/ImageModal.tsx
@@ -0,0 +1,17 @@
+import Modal from 'react-modal';
+import css from './ImageModal.module.css';
+import { FC } from 'react';
+
+type ImageModalProps = {
+  image: string;
+  state: boolean;
+  close: () => void;
+};
+
+export const ImageModal: FC<ImageModalProps> = ({ image, state, close }) => {
+  return (
+    <Modal isOpen={state} onRequestClose={close} className={css.modal}>
+      <img className={css.modalImg} src={image} />
+    </Modal>
+  );
+};
