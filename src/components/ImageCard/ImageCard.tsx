79c95d3e6ccb5c74779dcdc053673a77ce74dad6
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-06-01 18:36:01 +0300

    Create ImageCard.tsx

---
+++ src/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,28 @@
+import { Images } from '../../types';
+import css from './ImageCard.module.css';
+
+import React, { FC } from 'react';
+
+type ImageCardProps = {
+  img: Images;
+  onClick: (regular: string) => void;
+};
+
+export const ImageCard: FC<ImageCardProps> = ({
+  img: {
+    alt_description,
+    urls: { small, regular },
+  },
+  onClick,
+}) => {
+  return (
+    <div>
+      <img
+        onClick={() => onClick(regular)}
+        className={css.galleriImg}
+        src={small}
+        alt={alt_description}
+      />
+    </div>
+  );
+};
