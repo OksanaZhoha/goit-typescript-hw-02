5c5860144b1db9dd7ef17f669506e6f4b95a6d54
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-06-01 18:36:38 +0300

    Create LoadMoreBtn.tsx

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import css from './LoadMoreBtn.module.css';
+
+type LoadMoreBtnProps = {
+  addPage: () => void;
+};
+
+export const LoadMoreBtn: FC<LoadMoreBtnProps> = ({ addPage }) => {
+  return (
+    <div className={css.loadMoreBtnContainer}>
+      <button onClick={addPage} type="button" className={css.loadMoreBtn}>
+        Load more
+      </button>
+    </div>
+  );
+};
