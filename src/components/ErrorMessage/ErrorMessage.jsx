e5c301353b95617e98d7524cb26ec82bc4ea2e89
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-06-01 18:35:49 +0300

    Delete ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css';
-
-export default function ErrorMessage() {
-  return (
-    <div className={css.errorMessage}>
-      <p>Oops, sometsing wents wrong.Please try later</p>
-    </div>
-  );
-}
