c6e1c06695a41084feca31ac788235c3215526b8
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-06-01 18:36:07 +0300

    Create ImageGallery.tsx

---
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { Images } from '../../types';
+import { ImageCard } from '../ImageCard/ImageCard';
+import css from './ImageGallery.module.css';
+
+type PropsImageGallery = {
+  images: Images[];
+  onClick: (regular: string) => void;
+};
+
+export const ImageGallery: FC<PropsImageGallery> = ({ images, onClick }) => {
+  return (
+    <ul className={css.imageGalleryList}>
+      {images.map((img: Images) => (
+        <li key={img.id}>
+          <ImageCard img={img} onClick={onClick} />
+        </li>
+      ))}
+    </ul>
+  );
+};
